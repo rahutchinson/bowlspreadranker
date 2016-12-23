e5055c0550e10c65749507a05a9f41dba22c9d79
rahutchinson <ryanahutchinson@gmail.com>
2016-12-23 16:55:32 +0000

    added by day feature

---
+++ README.md
@@ -0,0 +1,18 @@
+
+     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.
+    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'
+    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, 
+    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.
+     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
+    ----------------------------------------------------------------- 
+
+
+This Project is a Python web scapper. 
+
+I wrote it to simplify the odds shark website to help with the
+ESPN Capital One Bowl Mania game.  
+
+It uses LXML and web.py to create a website with the games ranked by 
+spread.
+
+Every time the page is refreshed the scrape is completed and posted. 
--- ex50/bin/app.py
+++ ex50/bin/app.py
@@ -4,7 +4,8 @@ import web
 
 
 urls = (
-	'/', 'index'
+	'/', 'index',
+	'/byday', 'byday'
 )
 
 app = web.application(urls, globals())
@@ -16,17 +17,18 @@ from lxml.html import fromstring
 
 
 
-
-
 class Game(object):
 	team1 = None
 	team2 = None
 	winner = None
 	sprd = None
+	rank = None
+	day = None
 	
-	def __init__(self, team1, team2, sprd):
+	def __init__(self, team1, team2, sprd, day):
 		self.team1 = team1
 		self.team2 = team2
+		self.day = day
 		if sprd.text == 'Ev':
 			self.winner = None
 			self.sprd = float(0.0)
@@ -47,26 +49,28 @@ class Game(object):
 			return(None)
 			
 	def printing(self):
-		return("#%s Game: %s vs %s Spread: %s Winner: %s" % (self.rank, self.team1, self.team2, self.sprd,self.who_wins()))
+		return("#%s | Game: %s vs %s  | Spread: %s  | Winner: %s" % (self.rank, self.team1, self.team2, self.sprd,self.who_wins()))
+
+
+def get_web_objects(url,path):
+	content= urllib.urlopen(url).read()
+	doc = fromstring(content)
+	doc.make_links_absolute(url)	
+	objects = doc.xpath(path)
+	return objects
+
 
 class index:
-			
-	
 
-	
 	def GET(self):
 		url = 'http://www.oddsshark.com/ncaaf/odds'
 		
-		content= urllib.urlopen(url).read()
-		doc = fromstring(content)
-		doc.make_links_absolute(url)	
-		spread = doc.xpath("//div[contains(@class, 'op-spread')]")
-		
+		spread = get_web_objects(url,"//div[contains(@class, 'op-spread')]")
 		spread_list = []
 		count =0
 		for num in spread:
 			count+=1
-			if count == 1:
+			if count == 3:
 				spread_list.append(num)
 			elif count == 32:
 				count = 0
@@ -74,14 +78,15 @@ class index:
 		game_list = []
 		switch = 0
 		team_temp = None
-		team_names =  doc.xpath("//div[contains(@class, 'op-matchup-team')]//text()")
+		team_names =  get_web_objects(url, "//div[contains(@class, 'op-matchup-team')]//text()")
+		
 		for team in team_names:
 			switch+=1
 			if switch == 1:
 				team_temp = team
 			elif switch == 2:
 				switch = 0
-				game_list.append(Game(team_temp,team,spread_list[len(game_list)]))
+				game_list.append(Game(team_temp,team,spread_list[len(game_list)],0))
 				team_temp = None
 				
 		game_list.sort(key=lambda game: game.sprd, reverse=False)
@@ -97,6 +102,49 @@ class index:
 		form = web.input(name="Nobody", greet="Hello")
 		grt = "%s, %s" % (form.greet, form.name)
 		return render.index(greeting = grt)
+		
+		
+class byday:
+	
+	def GET(self):
+		url = 'http://www.oddsshark.com/ncaaf/odds'
+		
+		spread = get_web_objects(url,"//div[contains(@class, 'op-spread')]")
+		spread_list = []
+		count =0
+		for num in spread:
+			count+=1
+			if count == 3:
+				spread_list.append(num)
+			elif count == 32:
+				count = 0
+		
+		game_list = []
+		switch = 0
+		team_temp = None
+		team_names =  get_web_objects(url, "//div[contains(@class, 'op-matchup-team')]//text()")
+		day_count = 0
+		for team in team_names:
+			day_count+=1
+			switch+=1
+			if switch == 1:
+				team_temp = team
+			elif switch == 2:
+				switch = 0
+				game_list.append(Game(team_temp,team,spread_list[len(game_list)],day_count))
+				team_temp = None
+		
+		game_list.sort(key=lambda game: game.sprd, reverse=False)
+		count =0
+		for game in game_list: 
+			count+=1
+			game.rank = count	
+				
+		
+		game_list.sort(key=lambda game: game.day, reverse=False)
+		
+		
+		return render.byday(game_list)
 
 if __name__ == "__main__":
 	app.run()
